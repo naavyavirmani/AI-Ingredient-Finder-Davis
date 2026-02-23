551975bac244e9c18e4ae9a92594a385be057960
Naavya <148988293+naavyavirmani@users.noreply.github.com>
2026-02-23 13:02:55 -0800

    Update config.py

--- config.py
+++ config.py
@@ -13,3 +13,20 @@ It:
 Any value that might otherwise be hard-coded
 should live in this file.
 """
+DATA_FOLDER = "F1Data/"
+
+DRIVER_STANDINGS_FILE = DATA_FOLDER + "driver_standings.csv"
+RACES_FILE = DATA_FOLDER + "races.csv"
+
+MODEL_SAVE_PATH = "f1_season_model.pkl"
+
+
+TRAIN_UNTIL_YEAR = 2024  
+TARGET = "points"
+
+TEST_SIZE = 0.2
+RANDOM_SEED = 42
+
+
+N_ESTIMATORS = 200
+MAX_DEPTH = 8
